14033dacdd5c124ca2a6eccfbd2146b3af82b3f7
ggafiled <gafewkik234@gmail.com>
2021-08-04 16:42:19 +0700

    fix cronjob service

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('database:backup')
-            ->everyMinute()
+            ->daily()
             ->then(function () use ($schedule) {
                 Backup::create([
                     'chanel' => 'backup_log',
